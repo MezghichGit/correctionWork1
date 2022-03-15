e71c8b5a1680683041383402704f5b69270be79e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-03-15 15:21:36 +0100

    modif de amine

--- src/gestion_etudiant/model/Test.java
+++ src/gestion_etudiant/model/Test.java
@@ -28,6 +28,8 @@ public class Test {
 		
 		System.out.println("Vous avez taper : "+nom + " Age = "+age+ "Moyenne = "+moy+" Adresse = "+adress);
 		System.out.println("Update Achref Le 15/03/2022");
+		
+		System.out.println("Update Amine Le 15/03/2022 - 15h21");
 	}
 
 }
